6550224a27eb4fd9e6e3aa7745871a01b27ba355
suchetakumari <sucheta.hrk84@gmail.com>
2020-06-05 07:06:26 -0700

    2 files modified

--- src/app/test.directive.ts
+++ src/app/test.directive.ts
@@ -1,4 +1,4 @@
-import {Directive,ElementRef,HostListener,HostBinding} from '@angular/core';
+import {Directive,ElementRef,HostListener,HostBinding,Renderer} from '@angular/core';
 
 @Directive({
   selector: '[testDirective]'
@@ -6,15 +6,19 @@ import {Directive,ElementRef,HostListener,HostBinding} from '@angular/core';
 
 export class TestDirectiveComponent {
 
-  constructor(private elementRef:ElementRef){}
-@HostBinding('class.text-uppercase') private noShow:boolean=true;
+  constructor(private elementRef:ElementRef,private renderer:Renderer){}
+@HostBinding('class.text-uppercase') private noShow:boolean=false;
   @HostListener('mouseover') mouseover(){
-   this.elementRef.nativeElement.style.color='green';
+    //The ElementRef gives the directive direct access to the DOM element upon which it’s attached.
+  //  this.elementRef.nativeElement.style.color='green';
+  //Angular team has provided a platform independent way of setting properties on our elements via something called a Renderer.
+   this.renderer.setElementStyle(this.elementRef.nativeElement,'color','green')
    this.noShow=true;
   }
 
   @HostListener('mouseleave') leave(){
-   this.elementRef.nativeElement.style.color='red';
+  //  this.elementRef.nativeElement.style.color='red';
+  this.renderer.setElementStyle(this.elementRef.nativeElement,'color','red')
     this.noShow=false;
 
   }
