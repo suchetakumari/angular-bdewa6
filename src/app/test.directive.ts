3df85dd474b632288875d5f40504dccd3d9f0e8c
suchetakumari <sucheta.hrk84@gmail.com>
2020-06-05 07:09:24 -0700

    1 file modified

--- src/app/test.directive.ts
+++ src/app/test.directive.ts
@@ -7,7 +7,11 @@ import {Directive,ElementRef,HostListener,HostBinding,Renderer} from '@angular/c
 export class TestDirectiveComponent {
 
   constructor(private elementRef:ElementRef,private renderer:Renderer){}
+  //Using the @HostBinding decorator a directive can link an internal property to an input property on the host element. So if the internal property changed the input property on the host element would also change.
 @HostBinding('class.text-uppercase') private noShow:boolean=false;
+//Angular makes this easy with the @HostListener decorator.
+
+//This is a function decorator that accepts an event name as an argument. When that event gets fired on the host element it calls the associated function.
   @HostListener('mouseover') mouseover(){
     //The ElementRef gives the directive direct access to the DOM element upon which it’s attached.
   //  this.elementRef.nativeElement.style.color='green';
